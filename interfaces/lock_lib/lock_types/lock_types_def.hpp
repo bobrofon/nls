86fb3c551c472d33a4ce72da2feaaf334e64d319
bobrofon <bobrofon@gmail.com>
2014-01-30 03:31:51 +0700

    Initialization structure of the repository.

---
+++ interfaces/lock_lib/lock_types/lock_types_def.hpp
@@ -0,0 +1,10 @@
+#ifndef LOCK_TYPES_DEF_
+#define LOCK_TYPES_DEF_
+
+namespace LockLib {
+namespace LockTypes {
+
+} /* namespace LockTypes */
+} /* namespace LockLib */
+
+#endif /* LOCK_TYPES_DEF_ */
