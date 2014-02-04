2803c9888411edd146648f5367df1e433e0cb2ab
bobrofon <bobrofon@gmail.com>
2014-02-05 00:59:36 +0700

    done client without queries

--- modules/lock_service/src/lock_client.cpp
+++ modules/lock_service/src/lock_client.cpp
@@ -2,7 +2,7 @@
 #include "utils/config_interface.hpp"
 #include <boost/make_shared.hpp>
 
-#include <soci/sqlite3/soci-sqlite3.h> // stub
+#include <soci/mysql/soci-mysql.h> // stub
 
 using namespace soci;
 using namespace boost;
@@ -14,17 +14,18 @@ LockClient::LockClient() {
 	m_clientInfo = make_shared<ClientInfo>(0, m_sync.getEndpoint());
 
 	m_clientInfo->setEndpoint(m_sync.getEndpoint());
-	m_db.open(sqlite3, Config()->getStr("db.address"));
+	m_db.open(mysql, Config()->getStr("db.address"));
 	registerClient();
 }
 
 LockClient::LockClient(int _eid) {
 	m_clientInfo->setEndpoint(m_sync.getEndpoint());
-	m_db.open(sqlite3, Config()->getStr("db.address"));
+	m_db.open(mysql, Config()->getStr("db.address"));
 	registerClient(_eid);
 }
 
 LockClient::~LockClient() {
+	m_db << "delete from clients where id = :id", use(m_clientInfo->getId());
 	m_db.close();
 }
 
@@ -42,11 +43,35 @@ void LockClient::registerClient() {
 		use(static_cast<int> (m_clientInfo->getEndpoint().port()), "port");
 
 	int id = 0;
-	m_db << "select last_insert_rowid()", into(id);
+	m_db << "select id from clients where address = :address and port = :port",
+		use(m_clientInfo->getEndpoint().address().to_string(), "address"),
+		use(static_cast<int> (m_clientInfo->getEndpoint().port()), "port"),
+		into(id);
 	m_clientInfo = make_shared<ClientInfo>(id, id, m_clientInfo->getEndpoint());
-	m_db << "update clients set eid = :eid where id = :id", use(m_clientInfo->getEId(), "eid"), use(m_clientInfo->getId(), "id");
+	m_db << "update clients set eid = :eid where id = :id",
+		use(m_clientInfo->getEId(), "eid"),
+		use(m_clientInfo->getId(), "id");
 }
 
-void LockClient::registerClient(int /*_eid*/) {}
+void LockClient::registerClient(int _eid) {
+	m_db << "insert into clients(eid, address, port) values(:eid :address, :port)",
+		use(_eid, "eid"),
+		use(m_clientInfo->getEndpoint().address().to_string(), "address"), 
+		use(static_cast<int> (m_clientInfo->getEndpoint().port()), "port");
+
+	int id = 0;
+	use(m_clientInfo->getEndpoint().address().to_string(), "address"),
+	use(static_cast<int> (m_clientInfo->getEndpoint().port()), "port"),
+	into(id);
+	m_clientInfo = make_shared<ClientInfo>(id, _eid, m_clientInfo->getEndpoint());
+}
+
+LockClientInterface *newLockClient() {
+	return new LockClient();
+}
+
+LockClientInterface *newLockClient(int _eid) {
+	return new LockClient(_eid);
+}
 
 } /* namespace LockService */
